8bc82198d9330e69162e18732b9b128e0ea705ff
AshishSatpute <satpute2727@gmail.com>
2017-07-29 00:39:21 +0530

    IntercommunicationActivity

---
+++ interactivecommunication/src/main/java/com/example/interactivecommunication/ImageActivity.java
@@ -0,0 +1,71 @@
+package com.example.interactivecommunication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+
+public class ImageActivity extends AppCompatActivity {
+
+    private StringBuilder builder = new StringBuilder();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image);
+        Intent resInt = getIntent();
+        if(resInt != null) {
+            Bundle bundle = resInt.getExtras();
+            if(bundle != null) {
+                String imgNm = bundle.getString("imgNm");
+                if(imgNm.equals("a")) {
+                    ((ImageView)findViewById(R.id.imageView2)).setImageResource(R.drawable.ic_buses);
+                }
+                else if(imgNm.equals("b")){
+                    ((ImageView)findViewById(R.id.imageView2)).setImageResource(R.drawable.ic_cycle);
+                }
+                else {
+                    ((ImageView)findViewById(R.id.imageView2)).setImageResource(R.drawable.ic_airport_shuttle_black_24dp);
+                }
+            }
+        }
+
+        ((CheckBox)findViewById(R.id.cband)).setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b){
+                    builder.append("Android");
+                }
+            }
+        });
+
+
+        ((CheckBox)findViewById(R.id.cbapple)).setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b){
+                    builder.append("Apple");
+                }
+            }
+        });
+
+
+        ((CheckBox)findViewById(R.id.cbwin)).setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b){
+                    builder.append("Windows");
+                }
+            }
+        });
+    }
+
+    public void OnGoBack(View view){
+        Intent intent =  new Intent();
+        intent.putExtra("name",builder.toString());
+        setResult(RESULT_OK,intent);
+        finish();
+    }
+}
---
+++ interactivecommunication/src/main/java/com/example/interactivecommunication/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.interactivecommunication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void OnGoBack(View view) {
+        Class cls = ImageActivity.class;
+        Bundle bundle = new Bundle();
+        bundle.putString("imgNm",
+                ((EditText)(findViewById(R.id.edtext_image_name))).getText().toString());
+
+        Intent intent = new Intent(this, cls);
+        intent.putExtras(bundle);
+        startActivityForResult(intent,4546);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==4546 && resultCode==RESULT_OK && data!=null){
+            String name = data.getStringExtra("name");
+            ((TextView)findViewById(R.id.tv_information)).setText(name);
+        }
+    }
+}
