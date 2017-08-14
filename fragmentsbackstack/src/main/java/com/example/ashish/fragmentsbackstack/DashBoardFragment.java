bcbdf764ec1ebe183dfbd03b7fb080108326e1d8
AshishSatpute <satpute2727@gmail.com>
2017-08-14 13:09:22 +0530

    Fragments Back Stack

---
+++ fragmentsbackstack/src/main/java/com/example/ashish/fragmentsbackstack/DashBoardFragment.java
@@ -0,0 +1,40 @@
+package com.example.ashish.fragmentsbackstack;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DashBoardFragment extends Fragment {
+
+
+    public static Fragment getInstance(){
+        return new DashBoardFragment();
+    }
+
+    public DashBoardFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        final View rootView = inflater.inflate(R.layout.fragment_dash_board, container, false);
+
+        rootView.findViewById(R.id.btnNotify).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((MainActivity)getActivity()).loadFragment(NotificationFragment.getInstance());
+            }
+        });
+        return rootView;
+    }
+
+}
---
+++ fragmentsbackstack/src/main/java/com/example/ashish/fragmentsbackstack/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.ashish.fragmentsbackstack;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        loadFragment(LoginFragment.getInstance());
+    }
+
+    public void loadFragment(Fragment fragment){
+
+        String backStateName = fragment.getClass().getName();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        boolean fragmentPopped = fragmentManager.popBackStackImmediate(backStateName,0);
+        if (!fragmentPopped){
+            //
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.frame,fragment);
+            fragmentTransaction.addToBackStack(backStateName);
+            fragmentTransaction.commit();
+        }
+
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (getSupportFragmentManager().getBackStackEntryCount() == 1)
+            finish();
+        else
+            super.onBackPressed();
+    }
+}
