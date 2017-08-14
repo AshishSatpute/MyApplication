5d5b5b626fa790f76f30c27fe4fae0f683952b96
AshishSatpute <satpute2727@gmail.com>
2017-08-14 10:36:39 +0530

    ListView

---
+++ fragments/src/main/java/com/example/ashish/fragments/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.ashish.fragments;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void loadFrag(int img) {
+        FragmentManager mgr = getSupportFragmentManager();
+        FragmentTransaction txn = mgr.beginTransaction();
+
+        BlankFragment frag = new BlankFragment();
+        Bundle bundle = new Bundle();
+        bundle.putInt("img", img);
+        frag.setArguments(bundle);
+
+        txn.replace(R.id.frameLayout, frag);
+        txn.commit();
+    }
+}
