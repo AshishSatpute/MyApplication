8d8e02c40692014b6c0c6712b0a32980a28f8424
AshishSatpute <satpute2727@gmail.com>
2017-08-21 11:23:33 +0530

    Fragments Back Stack

---
+++ frag/src/main/java/com/example/ashish/frag/Chat_tabs.java
@@ -0,0 +1,22 @@
+package com.example.ashish.frag;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+public class Chat_tabs extends Fragment {
+
+    public Chat_tabs() {// Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.chat_tabs,container,false);
+    }
+
+}
---
+++ frag/src/main/java/com/example/ashish/frag/Status_tabs.java
@@ -0,0 +1,21 @@
+package com.example.ashish.frag;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Status_tabs extends Fragment {
+
+    public Status_tabs() {// Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.status_tabs,container,false);
+    }
+}
